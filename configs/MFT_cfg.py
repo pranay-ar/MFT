4c758f0ed60a7a9b2eb09c22a6aaa8db467aa49d
Pranay Reddy <a.pranay2001@gmail.com>
2024-09-05 15:00:18 -0700

    Update MFT_cfg.py

--- configs/MFT_cfg.py
+++ configs/MFT_cfg.py
@@ -15,7 +15,7 @@ def get_config():
     conf = Config()
 
     conf.tracker_class = MFT
-    conf.flow_config = load_config('external/manual_repos/MFT/mft/configs/flow/RAFTou_kubric_huber_split_nonoccl.py')
+    conf.flow_config = load_config('/GRID/external/manual_repos/MFT/mft/configs/flow/RAFTou_kubric_huber_split_nonoccl.py')
     conf.deltas = [np.inf, 1, 2, 4, 8, 16, 32]
     conf.occlusion_threshold = 0.02
 
